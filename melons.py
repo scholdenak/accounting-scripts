7db675e92f378e6cd157baf67c9dce454192ae81
vagrant <vagrant@vagrant.vm>
2020-10-06 06:15:47 +0000

    created melons dictionary

--- melons.py
+++ melons.py
@@ -1,8 +1,8 @@
 melons_data = {
-    'Honeydew': {'price': 0.99, 'seedless': True}
-    'Crenshaw': {'price': 2.00, 'seedless': False}
-    'Crane': {'price': 2.50, 'seedless': False}
-    'Casaba': {'price': 2.50, 'seedless': False}
+    'Honeydew': {'price': 0.99, 'seedless': True},
+    'Crenshaw': {'price': 2.00, 'seedless': False},
+    'Crane': {'price': 2.50, 'seedless': False},
+    'Casaba': {'price': 2.50, 'seedless': False},
     'Cantaloupe': {'price': 0.99, 'seedless': False}
 }
 
